d05584706a670a5fb4cd67a56daeaffed4144e7e
Nehal Verma <nehalverma17@gmail.com>
2024-07-20 13:44:07 +0530

    Update test_handler.py

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,24 +1,39 @@
+import json
+import datetime
 import unittest
-import lambda_function
 
+# Lambda function definition
+def lambda_handler(event, context):
+    print(event)
+    data = {
+        'output': 'Hello from ' + event['Country'],
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data),
+        'headers': {'Content-Type': 'application/json'}
+    }
 
+# Unit tests for the Lambda function
 class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
-        print("testing response.")
-        event={'Country':'USA'}
-        result = lambda_function.lambda_handler(event, None)
+        print("Testing response.")
+        
+        event = {'Country': 'USA'}
+        result = lambda_handler(event, None)
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello from '+event['Country'], result['body'])
-        event={'Country':'INDIA'}
-        result = lambda_function.lambda_handler(event, None)
-        print(result)
-        self.assertEqual(result['statusCode'], 200)
-        self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello from '+event['Country'], result['body'])
+        self.assertIn('Hello from ' + event['Country'], json.loads(result['body'])['output'])
 
+        event = {'Country': 'INDIA'}
+        result = lambda_handler(event, None)
+        print(result)
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        self.assertIn('Hello from ' + event['Country'], json.loads(result['body'])['output'])
 
 if __name__ == '__main__':
     unittest.main()
